cdfdfe249a0f62b0731045d5f6be152b59778849
Ansel Austin <ansel.austin@uri.edu>
2024-09-11 18:11:23 -0400

    temporary branch to fix localization issue

---
+++ wamv_bringup/scripts/path_sync_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import rospy
+from mvp_msgs.msg import Waypoint
+
+# from geometry_msgs.msg import PolygonStamped
+# from tf.transformations as tf_transform
+# import math
+# from mvp_msgs.srv import GetStateRequestm GetState, ChangeState, ChangeStateRequest
+# import time
+# import tf2_ros
+# import tf2_geometry_msgs
